d5f60b8622cd3591820b8b99da16e7c3cff0e8b5
Apurva Jyoti Paul <apurva721034@gmail.com>
2023-03-07 15:15:58 +0530

    Test

--- Iaas/setup.sh
+++ Iaas/setup.sh
@@ -26,6 +26,6 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 sudo usermod -aG docker ubuntu
 
-sudo docker run apurvajpaul/flipr:latest --restart=always
+sudo docker run --restart=always apurvajpaul/flipr:latest 
 
 
